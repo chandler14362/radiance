72fe84cbf9f1ffa6fab5484cba691a238771fed4
Chandler Stowell <chandler14362@gmail.com>
2017-08-28 12:56:57 -0400

    event: dispatch events by name

--- engine/event/dispatcher.asm
+++ engine/event/dispatcher.asm
@@ -1,20 +1,63 @@
 %include '../../engine/event/artifact.inc'
 %include '../../engine/type.inc'
 
+%ifndef MAX_EVENT_PARTICIPANTS
+    %xdefine MAX_EVENT_PARTICIPANTS 50
+%endif
+
 section .text
 
 extern radiance.event.members
+extern radiance.event.find
 global radiance.event.dispatch
 
-; dispatches an event
+; RADIANCE.EVENT.DISPATCH
+; dispatches an event 
 radiance.event.dispatch:
     prologue 16
 
-    mov eax, [radiance.event.members + EventParticipant.sub]
+    push esi
+    push ebx
+    push edi
 
-    ; call with args
-    mov edi, pointer_t [ebp + 8]
-    mov pointer_t [esp], edi
-    call eax
+    mov esi, [ebp + 8] ; name
+    mov ebx, [ebp + 12] ; args
+
+    ; get the event address.
+    mov pointer_t [esp], esi
+    call radiance.event.find
+
+    cmp eax, -1 ; check to see if the event exists
+    je .end
+
+    mov esi, int32_t [eax + RadianceEvent.id] ; copy the id over
+
+    ; iterate over all the members
+    xor ecx, ecx ; participant ptr
+    xor edi, edi ; loop count
+
+    .cmploop:
+        mov eax, [radiance.event.members + ecx + EventParticipant.id] ; check to see if the participant is listening for our event
+        cmp eax, esi
+        jne .inc_counters
+        
+        ; call with args
+        mov eax, [radiance.event.members + EventParticipant.sub]
+        mov pointer_t [esp], ebx
+        call eax
+
+    .inc_counters:
+        add ecx, EventParticipant.size ; inc pointer
+        inc edi
+
+        cmp edi, MAX_EVENT_PARTICIPANTS ; check if we have processed all events
+        je .end
+
+        jmp .cmploop ; loop again
+
+.end:
+    pop edi
+    pop ebx
+    pop esi
 
     epilogue 16
